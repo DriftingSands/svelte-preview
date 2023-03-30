4518ae7be8eeeeebf9b2d71b0684540b70c2100e
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2023-03-30 16:07:42 +0200

    css file and ts issues fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,8 @@
-<!-- <script>
-	export let data;
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-{#await data.props.data}
-	<p>loading...</p>
-{:then data}
-	{#each data as item}
-		<p>{item.name.first} {item.name.middle} {item.name.last}</p>
-	{/each}
-{/await} -->
-
 <script>
-	import { onMount, onDestroy } from 'svelte';
+// @ts-nocheck
+
+	import './styles.css'
+	import { onMount, } from 'svelte';
 	let data = null;
 	const dataHandler = (event) => {
 		console.log('\x1b[31m ~ event:', event);
