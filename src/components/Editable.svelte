675eb5db08ec594f587b1634c5f4b6709ca071e3
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2023-04-04 10:13:14 +0200

    added editable HOC for auto scrolling when opening tab in cf editor

---
+++ src/components/Editable.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount, onDestroy }  from 'svelte'
+  export let ref;
+  
+  function scrollTo(event) {
+    if (event?.data?.type !== 'scrollToPath' || event.data.path !== ref.dataset.editablePath) {return}
+    const box = ref.getBoundingClientRect();
+    if (box.top <= 0 && box.bottom >= window.innerHeight) {
+      return;
+    }
+    window.scrollBy({ top: box.top, left: 0, behavior: "smooth" });
+  }
+
+  onMount(() => window.addEventListener("message", scrollTo));
+  onDestroy(() => window.removeEventListener("message", scrollTo));
+</script>
+
+<slot />
--- src/components/Teaser.svelte
+++ src/components/Teaser.svelte
@@ -1,26 +1,31 @@
 <script>
-	// @ts-nocheck
+	import Editable from "./Editable.svelte";
 
 	export let teaser;
+
+	let teaserRef;
+
 </script>
 
-<div class="teaser" data-editable-path={teaser?._path}>
-	<div class="image">
-		<img
-			src={"https://author-p54352-e854610.adobeaemcloud.com" + teaser?.image._path}
-			alt={teaser?.imageAltText.plaintext}
-		/>
-	</div>
-	<div class="textContent">
-		<h4>{teaser?.title}</h4>
-		<p>{teaser?.description?.plaintext}</p>
-		<div class="buttons">
-			{#if teaser?.readMoreLink}
-				<a href={teaser?.readMoreLink} target="_blank">Read More</a>
-			{/if}
-			{#if teaser?.sourceLink}
-				<a href={teaser?.sourceLink} target="_blank">Source</a>
-			{/if}
+<Editable ref={teaserRef}>
+	<div class="teaser" data-editable-path={teaser?._path} bind:this={teaserRef} >
+		<div class="image">
+			<img
+				src={"https://author-p54352-e854610.adobeaemcloud.com" + teaser?.image._path}
+				alt={teaser?.imageAltText.plaintext}
+			/>
+		</div>
+		<div class="textContent">
+			<h4>{teaser?.title}</h4>
+			<p>{teaser?.description?.plaintext}</p>
+			<div class="buttons">
+				{#if teaser?.readMoreLink}
+					<a href={teaser?.readMoreLink} target="_blank">Read More</a>
+				{/if}
+				{#if teaser?.sourceLink}
+					<a href={teaser?.sourceLink} target="_blank">Source</a>
+				{/if}
+			</div>
 		</div>
 	</div>
-</div>
+</Editable>
